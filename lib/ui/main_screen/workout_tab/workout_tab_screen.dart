6dc478686236dddf7bac941723cf06c24b3e9cf0
Nicolas Vigouroux <nvigouroux@eig.fr>
2023-02-07 12:11:55 +0100

    change list architecture

--- lib/ui/main_screen/workout_tab/workout_tab_screen.dart
+++ lib/ui/main_screen/workout_tab/workout_tab_screen.dart
@@ -25,141 +25,134 @@ class _WorkoutTabScreenState extends State<WorkoutTabScreen> {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<WorkoutTabViewModel>.reactive(
-        viewModelBuilder: () => WorkoutTabViewModel(),
-        onModelReady: (model) {
-          EasyLoading.show();
-          model.initData(widget.date);
-          EasyLoading.dismiss(animation: false);
-        },
-        builder: (context, model, child) => Column(children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
-              ),
-              ChangeDateButtons(handleOnPressedLeftButton: (() async {
-                EasyLoading.show();
-                setState(() {
-                  widget.date = DateTime(
-                      widget.date.year, widget.date.month, widget.date.day - 1);
-                });
-                await model.loadData(widget.date);
-                widget.handleOnPressedDateButton(ChangeDateButtonTypeEnum.left);
-                EasyLoading.dismiss(animation: false);
-              }), handleOnPressedMiddleButton: (() async {
-                EasyLoading.show();
-                setState(() {
-                  widget.date = DateTime.now();
-                });
-                await model.loadData(widget.date);
-                widget
-                    .handleOnPressedDateButton(ChangeDateButtonTypeEnum.middle);
-                EasyLoading.dismiss(animation: false);
-              }), handleOnPressedRightButton: (() async {
-                EasyLoading.show();
-                setState(() {
-                  widget.date = DateTime(
-                      widget.date.year, widget.date.month, widget.date.day + 1);
-                });
-                await model.loadData(widget.date);
-                widget
-                    .handleOnPressedDateButton(ChangeDateButtonTypeEnum.right);
-                EasyLoading.dismiss(animation: false);
-              })),
-              Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                  child: MonitoringBox(
-                      date: widget.date,
-                      monitoring: model.monitoringDisplayed)),
-              SingleChildScrollView(
-                  child: SizedBox(
-                      height: MediaQuery.of(context).size.height - 375,
-                      child: ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          shrinkWrap: true,
-                          itemCount: model.workouts.length,
-                          itemBuilder: (context, index) {
-                            final workout = model.workouts[index];
-                            var subtitle = workout.notes ?? '';
-                            return Slidable(
-                                key: Key('workout-index-$index'),
-                                // The start action pane is the one at the left or the top side.
-                                startActionPane: ActionPane(
-                                    // A motion is a widget used to control how the pane animates.
-                                    motion: const ScrollMotion(),
-                                    children: [
-                                      SlidableAction(
-                                          onPressed: (BuildContext context) {
-                                            showDialog(
-                                                context: context,
-                                                builder:
-                                                    (BuildContext context) {
-                                                  return AlertDialog(
-                                                      title: const Text(
-                                                          "Supprimer"),
-                                                      content: Text(
-                                                          'Êtes vous sûr de vouloir supprimer la séance "${workout.name}"'),
-                                                      actions: <Widget>[
-                                                        ElevatedButton(
-                                                            onPressed:
-                                                                () async {
-                                                              EasyLoading
-                                                                  .show();
-                                                              await model
-                                                                  .deleteWorkout(
-                                                                      workout.id ??
-                                                                          0,
-                                                                      context);
-                                                              await model
-                                                                  .loadData(
-                                                                      widget
-                                                                          .date);
-                                                              EasyLoading
-                                                                  .dismiss(
-                                                                      animation:
-                                                                          false);
-                                                            },
-                                                            child: const Text(
-                                                                "Oui")),
-                                                        ElevatedButton(
-                                                            onPressed: () =>
-                                                                Navigator.of(
-                                                                        context)
-                                                                    .pop(false),
-                                                            child: const Text(
-                                                                "Non"))
-                                                      ]);
-                                                });
-                                          },
-                                          backgroundColor:
-                                              const Color(0xFFFE4A49),
-                                          foregroundColor: Colors.white,
-                                          icon: Icons.delete,
-                                          label: 'Supprimer')
-                                    ]),
-                                endActionPane: ActionPane(
-                                  motion: const ScrollMotion(),
-                                  children: [
-                                    SlidableAction(
-                                        onPressed: (BuildContext context) =>
-                                            model.openWorkoutScreen(
-                                                context, workout.id ?? 0),
-                                        backgroundColor:
-                                            const Color(0xFF7BC043),
-                                        foregroundColor: Colors.white,
-                                        icon: Icons.update,
-                                        label: 'Modifier')
-                                  ],
-                                ),
-                                child: ListTile(
-                                    title: Text(workout.name ?? ''),
-                                    subtitle: Text(subtitle)));
-                          }))),
+      viewModelBuilder: () => WorkoutTabViewModel(),
+      onModelReady: (model) {
+        EasyLoading.show();
+        model.initData(widget.date);
+        EasyLoading.dismiss(animation: false);
+      },
+      builder: (context, model, child) => Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+          ),
+          ChangeDateButtons(handleOnPressedLeftButton: (() async {
+            EasyLoading.show();
+            setState(() {
+              widget.date = DateTime(
+                  widget.date.year, widget.date.month, widget.date.day - 1);
+            });
+            await model.loadData(widget.date);
+            widget.handleOnPressedDateButton(ChangeDateButtonTypeEnum.left);
+            EasyLoading.dismiss(animation: false);
+          }), handleOnPressedMiddleButton: (() async {
+            EasyLoading.show();
+            setState(() {
+              widget.date = DateTime.now();
+            });
+            await model.loadData(widget.date);
+            widget.handleOnPressedDateButton(ChangeDateButtonTypeEnum.middle);
+            EasyLoading.dismiss(animation: false);
+          }), handleOnPressedRightButton: (() async {
+            EasyLoading.show();
+            setState(() {
+              widget.date = DateTime(
+                  widget.date.year, widget.date.month, widget.date.day + 1);
+            });
+            await model.loadData(widget.date);
+            widget.handleOnPressedDateButton(ChangeDateButtonTypeEnum.right);
+            EasyLoading.dismiss(animation: false);
+          })),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+            child: MonitoringBox(
+                date: widget.date, monitoring: model.monitoringDisplayed),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
               ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.blue.shade100)),
-                  onPressed: () => model.openWorkoutScreen(context, 0),
-                  child: const Text("Ajouter une séance",
-                      style: TextStyle(color: Colors.blue)))
-            ]));
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.blue.shade100),
+                ),
+                onPressed: () => model.openWorkoutScreen(context, 0),
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 5),
+          ),
+          ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            physics: ScrollPhysics(),
+            itemCount: model.workouts.length,
+            itemBuilder: (context, index) {
+              final workout = model.workouts[index];
+              var subtitle = workout.notes ?? '';
+              return Slidable(
+                key: Key('workout-index-$index'),
+                // The start action pane is the one at the left or the top side.
+                startActionPane: ActionPane(
+                    // A motion is a widget used to control how the pane animates.
+                    motion: const ScrollMotion(),
+                    children: [
+                      SlidableAction(
+                          onPressed: (BuildContext context) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text("Supprimer"),
+                                  content: Text(
+                                      'Êtes vous sûr de vouloir supprimer la séance "${workout.name}"'),
+                                  actions: <Widget>[
+                                    ElevatedButton(
+                                      onPressed: () async {
+                                        EasyLoading.show();
+                                        await model.deleteWorkout(
+                                            workout.id ?? 0, context);
+                                        await model.loadData(widget.date);
+                                        EasyLoading.dismiss(animation: false);
+                                      },
+                                      child: const Text("Oui"),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () =>
+                                          Navigator.of(context).pop(false),
+                                      child: const Text("Non"),
+                                    )
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                          backgroundColor: const Color(0xFFFE4A49),
+                          foregroundColor: Colors.white,
+                          icon: Icons.delete,
+                          label: 'Supprimer')
+                    ]),
+                endActionPane:
+                    ActionPane(motion: const ScrollMotion(), children: [
+                  SlidableAction(
+                      onPressed: (BuildContext context) =>
+                          model.openWorkoutScreen(context, workout.id ?? 0),
+                      backgroundColor: const Color(0xFF7BC043),
+                      foregroundColor: Colors.white,
+                      icon: Icons.update,
+                      label: 'Modifier')
+                ]),
+                child: ListTile(
+                  title: Text(workout.name ?? ''),
+                  subtitle: Text(subtitle),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
